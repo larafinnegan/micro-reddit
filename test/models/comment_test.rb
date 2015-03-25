5f17b862a6734b84821ff869fdeba6353121b72b
Lara Finnegan <lcfinnegan@gmail.com>
2015-03-24 23:25:57 -0400

    added tests for comments model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@comment = Comment.new(body: "Body", user_id: 1, post_id: 1)
+end
+
+test "body cannot be blank" do 
+	@comment.body = "    "
+	assert_not @comment.valid?
+end
+
+test "user id cannot be blank" do 
+	@comment.user_id = nil
+	assert_not @comment.valid?
+end
+
+test "post_id" do 
+	@comment.post_id = nil
+	assert_not @comment.valid?
+end
 end
